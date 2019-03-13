f5f34817d5f2d9d77fe7bf7e188dc68b92d2d41f
Joonas Kämäräinen <joonas@ninjami.fi>
2019-03-13 18:38:05 +0200

    Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Ninjami</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -47,7 +47,9 @@
             .title {
                 font-size: 84px;
             }
-
+            .links {
+                margin-top: 25px;
+            }
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -81,17 +83,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ninjami Zippiläinen mies
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/api/zips">All zips</a>
+                    <a href="/api/areas">All areas</a>
+                </div>
+                <div class="links">
+                    <a href="https://github.com/joonas1234/zip-area">Source and "documentation"</a>
                 </div>
             </div>
         </div>
